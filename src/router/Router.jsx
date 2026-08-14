48f860eb490e28229c623ee2bea96e814ba28f28
Herval Mata <hervalmataamparo@gmail.com>
2026-08-14 19:33:41 -0300

    :sparkles: feat: new ecommerce - login register page state define

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,8 +1,7 @@
-import { useRoutes } from 'react-router-dom'
+import {useRoutes} from 'react-router-dom'
 
 const Router = ({ allRoutes}) => {
-    const routes = useRoutes([...allRoutes])
-    return routes;
+    return useRoutes([...allRoutes]);
 }
 
 export default Router
