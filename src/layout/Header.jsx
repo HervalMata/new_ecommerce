f521a4fc7afe03fdc516e5ec2389671bd12d92ff
Herval Mata <hervalmataamparo@gmail.com>
2026-08-15 13:57:43 -0300

    :sparkles: feat: new ecommerce - project dashboard design

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,32 @@
-const Header = () => {
+import {FaList} from "react-icons/fa";
+
+const Header = ({ showSidebar, setShowSidebar }) => {
     return (
-        <div>
-            <h1>Header</h1>
+        <div className="fixed top-0 left-0 w-full py-5 px-2 lg:px-7 z-40">
+            <div className="ml-0 lg:ml-[260px] rounded-md h-[65px] flex items-center justify-between bg-[#B1ADDF] px-5
+            transition-all">
+                <div onClick={() => setShowSidebar(!showSidebar)} className="flex lg:hidden w-[35px] h-[35px] rounded-sm
+                bg-indigo-500 shadow-lg hover:shadow-indigo-500/50 justify-center items-center cursor-pointer">
+                    <span><FaList /></span>
+                </div>
+                <div className="hidden md:block">
+                    <input type="text" name="search" placeholder="Procurar..." className='px-3 py-2 outline-none border
+                        bg-transparent border-slate-700 rounded-md text-[#423D72] focus:border-indigo-5000
+                        overflow-hidden' />
+                </div>
+                <div className="flex items-center justify-center gap-8 relative">
+                    <div className="flex items-center justify-center">
+                        <div className="flex items-center justify-center gap-3">
+                            <div className="flex items-center justify-center flex-col text-end">
+                                <h2 className="text-md font-bold">Herval Mata</h2>
+                                <span className="text-[14px] w-full font-normal">Admin</span>
+                            </div>
+                            <img className="w-[45px] h-[45px] rounded-full overflow-hidden"
+                                 src="http://localhost:3000/images/admin.jpg" alt="imagem" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -3,7 +3,7 @@ import {useEffect, useState} from "react";
 import {getNav} from "../navigation";
 import {BiLogOutCircle} from "react-icons/bi";
 
-const Sidebar = () => {
+const Sidebar = ({ showSidebar, setShowSidebar }) => {
     const {pathname} = useLocation()
     const [allNav, setAllNav] = useState([]);
 
@@ -15,8 +15,10 @@ const Sidebar = () => {
 
     return (
         <div>
-            <div></div>
-            <div className={`w-[260px] fixed bg-[#E6E7FB] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all`}>
+            <div onClick={() => setShowSidebar(false)} className={`fixed duration-200 ${!showSidebar ? 
+                'invisible' : 'visible'} w-screen h-screen bg-[#8CBCE780] top-0 left-0 z-10`}></div>
+            <div className={`w-[260px] fixed bg-[#E6E7FB] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] 
+                             transition-all ${showSidebar ? 'left-0' : '-left-[260px] lg:left-0'}`}>
                 <div className="h-[70px] flex items-center justify-center">
                     <Link to="/" className="w-[180px] h-[120px]">
                         <img className="w-full h-full" src="http://localhost:3000/images/logo.png" alt="imagem" />
@@ -30,7 +32,8 @@ const Sidebar = () => {
                                     `${pathname === nav.path 
                                         ? 'bg-blue-600 shadow-indigo-500/50 text-white duration-500' 
                                         : 'text-[#030811] font-bold duration-200'
-                                    } px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1
+                                    } px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] 
+                                         hover:pl-4 transition-all w-full mb-1
                                     `}
                                     to={nav.path}>
                                     <span>{nav.icon}</span>
@@ -39,7 +42,8 @@ const Sidebar = () => {
                             </li>
                         )}
                         <li>
-                            <button className="text-[#030811] font-bold duration-200 px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1">
+                            <button className="text-[#030811] font-bold duration-200 px-[12px] py-[9px] rounded-sm
+                                     flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1">
                                 <span><BiLogOutCircle /></span>
                                 <span>Sair</span>
                             </button>
