8c2d0b43f359ac4fadd6514d2d5b7e9c855620e4
Herval Mata <hervalmataamparo@gmail.com>
2026-08-15 17:27:33 -0300

    :sparkles: feat: new ecommerce - admin order oage and pagination design

--- src/router/routes/adminRoutes.js
+++ src/router/routes/adminRoutes.js
@@ -1,9 +1,15 @@
 import AdminDashboard from "../../views/admin/AdminDashboard";
+import Orders from "../../views/admin/Orders";
 
 export const adminRoutes = [
     {
         path: '/admin/dashboard',
         element: <AdminDashboard />,
+        role: 'admin',
+    },
+    {
+        path: '/admin/dashboard/orders',
+        element: <Orders />,
         role: 'admin',
     }
 ]
