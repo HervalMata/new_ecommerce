1c52a120b6cd7daf659e3b99b128d8ad590e3b83
Herval Mata <hervalmataamparo@gmail.com>
2026-08-16 20:41:13 -0300

    :sparkles: feat: new ecommerce - add product page design

--- src/router/routes/sellerRoutes.js
+++ src/router/routes/sellerRoutes.js
@@ -1,14 +1,21 @@
 import Home from "../../views/Home";
 import SellerDashboard from "../../views/seller/SellerDashboard";
+import AddProduct from "../../views/seller/AddProduct";
 
 export const sellerRoutes = [
     {
         path: '/',
         element: <Home />,
         ability: ['admin', 'seller'],
-    },{
+    },
+    {
         path: '/seller/dashboard',
         element: <SellerDashboard />,
+        ability: ['seller'],
+    },
+    {
+        path: '/seller/dashboard/add-product',
+        element: <AddProduct />,
         ability: ['seller'],
     }
 ]
